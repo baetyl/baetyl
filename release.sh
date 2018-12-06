bdc8df81dcf990a2d2609207b3144bc96117ccdf
wangmengtao <wangmengtao@baidu.com>
2018-12-06 19:44:08 +0800

    update release.sh && fix some example document

--- release.sh
+++ release.sh
@@ -44,30 +44,36 @@ build() {
     fi
     mkdir -p output/$TAG
 
-    tar cf - -C example/native . | tar xf - -C output/$TAG
+    # docker release for multiple cpu && os
+    tar cf - -C example/docker . | tar xf - -C output/$TAG
+
+    # native release for multiple cpu && os(enable comment script below)
+    # tar cf - -C example/native . | tar xf - -C output/$TAG
 
     # engine
     env GOOS=$GOOS GOARCH=$GOARCH go build -o output/$TAG/bin/openedge$GOEXE ./cmd
 
     # modules
-    env GOOS=$GOOS GOARCH=$GOARCH go build -o output/$TAG/bin/openedge_hub$GOEXE ./module/hub/cmd
-    env GOOS=$GOOS GOARCH=$GOARCH go build -o output/$TAG/bin/openedge_function$GOEXE ./module/function/cmd
-    env GOOS=$GOOS GOARCH=$GOARCH go build -o output/$TAG/bin/openedge_remote_mqtt$GOEXE ./module/remote/mqtt
+    # env GOOS=$GOOS GOARCH=$GOARCH go build -o output/$TAG/bin/openedge_hub$GOEXE ./module/hub/cmd
+    # env GOOS=$GOOS GOARCH=$GOARCH go build -o output/$TAG/bin/openedge_function$GOEXE ./module/function/cmd
+    # env GOOS=$GOOS GOARCH=$GOARCH go build -o output/$TAG/bin/openedge_remote_mqtt$GOEXE ./module/remote/mqtt
 
     # function runtime python2.7
-    cp module/function/runtime/python2.7/*.py  output/$TAG/bin/
-    chmod +x output/$TAG/bin/openedge_function_runtime_python2.7.py
+    # cp module/function/runtime/python2.7/*.py  output/$TAG/bin/
+    # chmod +x output/$TAG/bin/openedge_function_runtime_python2.7.py
 
     cd output/$TAG/
     tar czvf ../openedge-$TAG-$version.tar.gz *
     cd ../../
 }
 
-# build for multi CPU && os, if for windows, please install `mingw` first
-build   "linux-x86"       "linux"     "386"     "cc"   
-build   "linux-x86_64"    "linux"     "amd64"   "cc"     
-build   "linux-armv7"     "linux"     "arm"     "cc"
-build   "linux-aarch64"   "linux"     "arm64"   "cc"
-# build   "darwin-x86_64"   "darwin"    "amd64"   "cc"
-build   "windows-x86"     "windows"   "386"     "i686-w64-mingw32-gcc"
-build   "windows-x86_64"  "windows"   "amd64"   "x86_64-w64-mingw32-gcc"
+# build for multiple cpu && os, if for windows, please install `mingw` first
+build   "linux-x86"         "linux"     "386"     "cc"   
+build   "linux-x86_64"      "linux"     "amd64"   "cc"  
+build   "linux-armv7"       "linux"     "arm"     "cc"
+build   "linux-aarch64"     "linux"     "arm64"   "cc"
+build   "windows10-x86"     "windows"   "386"     "i686-w64-mingw32-gcc"
+build   "windows10-x86_64"  "windows"   "amd64"   "x86_64-w64-mingw32-gcc"
+
+# must be built on MacOS
+# build   "darwin-x86_64"     "darwin"    "amd64"   "cc"
