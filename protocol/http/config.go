390e905b963c7f5fe18e77ed2dfb2711b03fdc6d
陆丹峰 <killucs@qq.com>
2019-05-07 18:53:54 -0700

    increase the timeout of http server and client (#225)

--- protocol/http/config.go
+++ protocol/http/config.go
@@ -9,15 +9,15 @@ import (
 // ServerInfo http server config
 type ServerInfo struct {
 	Address           string        `yaml:"address" json:"address"`
-	Timeout           time.Duration `yaml:"timeout" json:"timeout" default:"30s"`
+	Timeout           time.Duration `yaml:"timeout" json:"timeout" default:"5m"`
 	utils.Certificate `yaml:",inline" json:",inline"`
 }
 
 // ClientInfo http client config
 type ClientInfo struct {
 	Address           string        `yaml:"address" json:"address"`
-	Timeout           time.Duration `yaml:"timeout" json:"timeout" default:"30s"`
-	KeepAlive         time.Duration `yaml:"keepalive" json:"keepalive" default:"60s"`
+	Timeout           time.Duration `yaml:"timeout" json:"timeout" default:"5m"`
+	KeepAlive         time.Duration `yaml:"keepalive" json:"keepalive" default:"10m"`
 	Username          string        `yaml:"username" json:"username"`
 	Password          string        `yaml:"password" json:"password"`
 	utils.Certificate `yaml:",inline" json:",inline"`
