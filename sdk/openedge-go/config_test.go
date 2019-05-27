87d990c39c79d8a86b98ca1c0bcff39fea2a81d3
sherlockblaze <sherlockblaze@gmail.com>
2019-05-27 15:16:47 +0800

    add unit-test for config full cover

--- sdk/openedge-go/config_test.go
+++ sdk/openedge-go/config_test.go
@@ -84,7 +84,48 @@ volumes:
 
 var cfgV3 = `
 version: V5
-  `
+`
+
+var cfgV4 = `
+version: V5
+services:
+  - name: agent
+    image: '  hub.baidubce.com/iottest/openedge-agent:latest'
+    replica: 1
+    mounts:
+      - name: agent-conf-lruvr02ct-V2
+        path: etc/openedge
+        readonly: true
+      - name: agent-cert-lruvr02ct-V1
+        path: var/db/openedge/cert
+        readonly: true
+      - name: agent-volumes-lruvr02ct-V1
+        path: var/db/openedge/volumes
+      - name: agent-log-lruvr02ct-V1
+        path: var/log/openedge
+      - name: openedge-agent-bin-linux-amd64-V3
+        path: lib/openedge/hub.baidubce.com/iottest/openedge-agent:latest
+        readonly: true
+  - name: hub
+    image: 'hub.baidubce.com/iottest/openedge-hub:latest'
+    replica: 1
+    mounts:
+      - name: hub-conf-V1
+        path: etc/openedge
+volumes:
+  - name: agent-conf-lruvr02ct-V2
+    path: var/db/openedge/agent-conf-lruvr02ct/V2
+  - name: agent-cert-lruvr02ct-V1
+    path: var/db/openedge/agent-cert-lruvr02ct/V1
+  - name: agent-volumes-lruvr02ct-V1
+    path: var/db/openedge
+  - name: agent-log-lruvr02ct-V1
+    path: var/db/openedge/agent-log
+  - name: openedge-agent-bin-linux-amd64-V3
+    path: var/db/openedge/openedge-agent-bin-linux-amd64/V3
+  - name: hub-conf-V1
+    path: var/db/openedge/openedge-hub-config/V1
+`
 
 func TestAppConfigValidate(t *testing.T) {
 	var cfgObj AppConfig
@@ -105,13 +146,19 @@ func TestDiffServices(t *testing.T) {
 	err = utils.UnmarshalYAML([]byte(cfgV3), &V3)
 	assert.NoError(t, err)
 
+	var V4 AppConfig
+	err = utils.UnmarshalYAML([]byte(cfgV4), &V4)
+	assert.NoError(t, err)
+
 	servicesV1 := V1.Services
 	servicesV2 := V2.Services
 	servicesV3 := V3.Services
+	servicesV4 := V4.Services
 
 	volumesV1 := V1.Volumes
 	volumesV2 := V2.Volumes
 	volumesV3 := V3.Volumes
+	volumesV4 := V4.Volumes
 
 	serviceMap := make(map[string]ServiceInfo)
 	for _, service := range servicesV1 {
@@ -187,6 +234,31 @@ func TestDiffServices(t *testing.T) {
 
 	updatedVolumes, _ = DiffVolumes(volumesV3, volumesV2)
 	updated, removed = DiffServices(servicesV3, servicesV2, updatedVolumes)
+	if !reflect.DeepEqual(removed, removedWant) {
+		t.Errorf("DiffServices () = %v, want %v", removed, removedWant)
+	}
+	if !reflect.DeepEqual(updated, updatedWant) {
+		t.Errorf("DiffServices () = %v, want %v", updated, updatedWant)
+	}
+
+	for _, service := range servicesV2 {
+		serviceMap[service.Name] = service
+	}
+
+	removedWant = []ServiceInfo{
+		serviceMap["agent"],
+	}
+
+	for _, service := range servicesV4 {
+		serviceMap[service.Name] = service
+	}
+
+	updatedWant = []ServiceInfo{
+		serviceMap["agent"],
+	}
+
+	updatedVolumes, _ = DiffVolumes(volumesV2, volumesV4)
+	updated, removed = DiffServices(servicesV2, servicesV4, updatedVolumes)
 	if !reflect.DeepEqual(removed, removedWant) {
 		t.Errorf("DiffServices () = %v, want %v", removed, removedWant)
 	}
