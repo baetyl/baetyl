19872534081e143737cbb8aad00c202635957915
chensheng06 <chensheng06@baidu.com>
2019-10-14 20:36:17 +0800

    bce-iot-10386: support mac platform in scripts of online install

--- scripts/install.sh
+++ scripts/install.sh
@@ -1,12 +1,20 @@
-#!/bin/sh
+#!/bin/bash
 
 set -e
 
+workdir=$(
+    cd $(dirname $0)
+    pwd
+)
+cd $workdir
+
 PACKAGE_NAME=baetyl
+VERSION=0.1.6
 URL_PACKAGE=dl.baetyl.io
 URL_KEY=http://${URL_PACKAGE}/key.public
-LSB_DIST=$(. /etc/os-release && echo "$ID")
-PRE_INSTALL_PKGS=""
+OS=$(uname)
+PRE_INSTALL_PKGS="ca-certificates"
+EFFECTIVE_UID=$("id" | grep -o "uid=[0-9]*" | cut -d= -f2)
 
 print_status() {
     echo
@@ -14,57 +22,73 @@ print_status() {
     echo
 }
 
-if [ ! -x /usr/bin/gpg ]; then
+exec_cmd_nobail() {
+    echo "+ sh -c '$1'"
+    bash -c "$1"
+}
+
+if [ $EFFECTIVE_UID -ne 0 ]; then
+    print_status "The script needs to be run as root."
+    exit 1
+fi
+
+if [ -x "$(command -v gpg)" ]; then
     PRE_INSTALL_PKGS="${PRE_INSTALL_PKGS} gnupg"
 fi
 
-if [ $LSB_DIST = centos ]; then
-
-    if [ "X${PRE_INSTALL_PKGS}" != "X" ]; then
-        yum install -y ${PRE_INSTALL_PKGS} >/dev/null 2>&1
-    fi
-
-    {
-        echo '[baetyl]'
-        echo 'name=baetyl'
-        echo "baseurl=http://${URL_PACKAGE}/linux/centos/7/x86_64"
-        echo 'gpgcheck=1'
-        echo 'enabled=1'
-        echo "gpgkey=$URL_KEY"
-    } >>/etc/yum.repos.d/baetyl.repo
-
-    yum install -y $PACKAGE_NAME
-    systemctl enable $PACKAGE_NAME
+if [ ${OS} = Darwin ]; then
+    exec_cmd_nobail "curl http://${URL_PACKAGE}/mac/static/x86_64/baetyl-${VERSION}-darwin-amd64.tar.gz | tar xvzf - -C /usr/local"
 else
+    LSB_DIST=$(. /etc/os-release && echo "$ID")
 
-    if [ ! -x /usr/bin/lsb_release ]; then
-        PRE_INSTALL_PKGS="${PRE_INSTALL_PKGS} lsb-release"
+    if [ ${LSB_DIST} = centos ]; then
+        PRE_INSTALL_PKGS="${PRE_INSTALL_PKGS} yum-utils"
+        YUM_REPO="http://${URL_PACKAGE}/linux/$LSB_DIST/baetyl.repo"
+
+        if [ "X${PRE_INSTALL_PKGS}" != "X" ]; then
+            exec_cmd_nobail "yum install -y ${PRE_INSTALL_PKGS}"
+        fi
+
+        if ! curl -Ifs "$YUM_REPO" >/dev/null; then
+            print_status "Error: Unable to curl repository file $YUM_REPO, is it valid?"
+            exit 1
+        fi
+
+        exec_cmd_nobail "yum-config-manager --add-repo $YUM_REPO"
+        exec_cmd_nobail "yum makecache"
+
+        exec_cmd_nobail "yum install -y ${PACKAGE_NAME}"
+        exec_cmd_nobail "systemctl enable ${PACKAGE_NAME}"
+    else
+        if [ -x "$(command -v lsb_release)" ]; then
+            PRE_INSTALL_PKGS="${PRE_INSTALL_PKGS} lsb-release"
+        fi
+
+        if [ -x "$(command -v curl)" ]; then
+            PRE_INSTALL_PKGS="${PRE_INSTALL_PKGS} curl"
+        fi
+
+        if [ ! -e /usr/lib/apt/methods/https ]; then
+            PRE_INSTALL_PKGS="${PRE_INSTALL_PKGS} apt-transport-https"
+        fi
+
+        if [ "X${PRE_INSTALL_PKGS}" != "X" ]; then
+            exec_cmd_nobail "apt-get update"
+            exec_cmd_nobail "apt-get install -y ${PRE_INSTALL_PKGS} >/dev/null 2>&1"
+        fi
+
+        exec_cmd_nobail "echo \"deb http://${URL_PACKAGE}/linux/${LSB_DIST} $(lsb_release -cs) main\" |
+        tee /etc/apt/sources.list.d/${PACKAGE_NAME}.list"
+
+        exec_cmd_nobail "curl -fsSL ${URL_KEY} | apt-key add -"
+
+        print_status "Added sign key!"
+
+        exec_cmd_nobail "apt update"
+        exec_cmd_nobail "apt install ${PACKAGE_NAME}"
     fi
-
-    if [ ! -x /usr/bin/curl ]; then
-        PRE_INSTALL_PKGS="${PRE_INSTALL_PKGS} curl"
-    fi
-
-    if [ ! -e /usr/lib/apt/methods/https ]; then
-        PRE_INSTALL_PKGS="${PRE_INSTALL_PKGS} apt-transport-https"
-    fi
-
-    if [ "X${PRE_INSTALL_PKGS}" != "X" ]; then
-        apt-get update
-        apt-get install -y ${PRE_INSTALL_PKGS} >/dev/null 2>&1
-    fi
-
-    echo "deb http://${URL_PACKAGE}/linux/$(lsb_release -is | tr 'A-Z' 'a-z') $(lsb_release -cs) main" |
-        tee /etc/apt/sources.list.d/${PACKAGE_NAME}.list
-
-    curl -fsSL $URL_KEY | apt-key add -
-
-    print_status "Added sign key!"
-
-    apt update
-    apt install $PACKAGE_NAME
 fi
 
-print_status "Install $PACKAGE_NAME Successfully!"
+print_status "Install ${PACKAGE_NAME} Successfully!"
 
 exit 0
--- scripts/install_with_docker_example.sh
+++ scripts/install_with_docker_example.sh
@@ -1,18 +1,60 @@
-#!/bin/sh
+#!/bin/bash
 
 set -e
 
+workdir=$(
+    cd $(dirname $0)
+    pwd
+)
+cd $workdir
+
 PACKAGE_NAME=baetyl
-EXAMPLE_PATH=http://dl.baetyl.io/example/docker
+VERSION=0.1.6
+EXAMPLE_PATH=http://dl.baetyl.io/example/${VERSION}/docker
+OS=$(uname)
 
-systemctl stop $PACKAGE_NAME
+print_status() {
+    echo
+    echo "## $1"
+    echo
+}
 
-rm -rf /usr/local/var/db/baetyl
-rm -rf /usr/local/etc/baetyl
+exec_cmd_nobail() {
+    echo "+ $2 sh -c '$1'"
+    $2 bash -c "$1"
+}
 
-curl -O $EXAMPLE_PATH/docker_example.tar.gz
-tar -C /usr/local -xzf docker_example.tar.gz
+if ls /usr/local/var/db/baetyl >/dev/null 2>&1; then
+    read -p "There are outdated configurations. Do you want to remove or not (y/n, default: y): " v
+    if [[ "${v}" == "n" || "${v}" == "N" || "${v}" == "no" || "${v}" == "NO" || "${v}" == "No" ]]; then
+        print_status "This script will exit now..."
+        exit 1
+    fi
+    exec_cmd_nobail "rm -rf /usr/local/etc/baetyl /usr/local/var/db/baetyl" "sudo"
+fi
 
-systemctl start $PACKAGE_NAME
+if [ ${OS} = Darwin ]; then
+    # docker for mac runs as a non-root user
+    TEMPDIR=$(mktemp -d)
+    exec_cmd_nobail "curl $EXAMPLE_PATH/docker_example.tar.gz | tar xvzf - -C ${TEMPDIR}"
+    exec_cmd_nobail "tar cf - -C ${TEMPDIR} etc var | tar xvf - -C /usr/local/" "sudo"
+    rm -rf ${TEMPDIR}
+else
+    exec_cmd_nobail "curl $EXAMPLE_PATH/docker_example.tar.gz | tar xvzf - -C /usr/local/" "sudo"
+fi
+
+print_status "Import the example configuration Successfully!"
+
+read -p "Baetyl functional modules are released as docker images. Do you want to pull required images now (y/n, default: y): " v
+if [[ "${v}" == "n" || "${v}" == "N" || "${v}" == "no" || "${v}" == "NO" || "${v}" == "No" ]]; then
+    exit 0
+fi
+
+TARGETS=(hub function-manager function-python27 function-python36 function-node85 function-sql timer)
+for target in ${TARGETS[@]}; do
+    docker pull hub.baidubce.com/baetyl/baetyl-${target}
+done
+
+print_status "Pulled required images Successfully!"
 
 exit 0
