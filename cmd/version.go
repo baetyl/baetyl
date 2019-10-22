8252d67c99756573e8abb5ff91f8ad2d7532e471
chensheng06 <chensheng06@baidu.com>
2019-10-22 15:21:17 +0800

    bce-iot-10568: add platform info

--- cmd/version.go
+++ cmd/version.go
@@ -3,6 +3,10 @@ package cmd
 import (
 	"fmt"
 	"runtime"
+	"strings"
+
+	"github.com/containerd/containerd/platforms"
+	specs "github.com/opencontainers/image-spec/specs-go/v1"
 
 	"github.com/spf13/cobra"
 )
@@ -11,6 +15,7 @@ import (
 var (
 	Version  string
 	Revision string
+	Platform specs.Platform
 )
 
 // versionCmd represents the version command
@@ -23,8 +28,30 @@ var versionCmd = &cobra.Command{
 
 func init() {
 	rootCmd.AddCommand(versionCmd)
+	Platform = platforms.DefaultSpec()
 }
 
 func version(cmd *cobra.Command, args []string) {
-	fmt.Printf("Version:      %s\nGit revision: %s\nGo version:   %s\n\n", Version, Revision, runtime.Version())
+	fmt.Printf("Version:      %s\nGit revision: %s\nGo version:   %s\nPlatform:     %s\n\n", Version, Revision, runtime.Version(), format(Platform))
+}
+
+func format(platform specs.Platform) string {
+	if platform.OS == "" {
+		return "unknown"
+	}
+
+	return joinNotEmpty(platform.OS, platform.Architecture, platform.Variant)
+}
+
+func joinNotEmpty(s ...string) string {
+	var ss []string
+	for _, s := range s {
+		if s == "" {
+			continue
+		}
+
+		ss = append(ss, s)
+	}
+
+	return strings.Join(ss, "/")
 }
