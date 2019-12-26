951db1e228dffbb5f69d11ed6476f8d15f020286
fanzehui <fanzehui@baidu.com>
2019-12-26 19:12:51 +0800

    timer

--- baetyl-timer/main.go
+++ baetyl-timer/main.go
@@ -1,8 +1,10 @@
 package main
 
 import (
-	"encoding/json"
+	"bytes"
 	"fmt"
+	"math/rand"
+	"text/template"
 	"time"
 
 	"github.com/256dpi/gomqtt/packet"
@@ -15,10 +17,39 @@ type config struct {
 		Interval time.Duration `yaml:"interval" json:"interval" default:"1m"`
 	} `yaml:"timer" json:"timer"`
 	Publish struct {
-		QOS     uint32                 `yaml:"qos" json:"qos" validate:"min=0, max=1"`
-		Topic   string                 `yaml:"topic" json:"topic" default:"timer" validate:"nonzero"`
-		Payload map[string]interface{} `yaml:"payload" json:"payload" default:"{}"`
+		QOS     uint32 `yaml:"qos" json:"qos" validate:"min=0, max=1"`
+		Topic   string `yaml:"topic" json:"topic" default:"timer" validate:"nonzero"`
+		Payload string `yaml:"payload" json:"payload" default:"{}"`
 	} `yaml:"publish" json:"publish"`
+}
+
+type cx struct {
+	Time  Time
+	Rand  Rand
+	Frand Frand
+}
+
+type Time struct {
+}
+
+type Rand struct {
+}
+
+type Frand struct {
+}
+
+func (t *Time) TE() int64 {
+	return time.Now().UnixNano()
+}
+
+func (r *Rand) RD() int {
+	rand.Seed(time.Now().UnixNano())
+	return rand.Intn(100)
+}
+
+func (f *Frand) FD() float32 {
+	rand.Seed(time.Now().UnixNano())
+	return 60 * rand.Float32()
 }
 
 func main() {
@@ -42,16 +73,25 @@ func main() {
 		defer ticker.Stop()
 		for {
 			select {
-			case t := <-ticker.C:
-				cfg.Publish.Payload["time"] = t.Unix()
-				pld, err := json.Marshal(cfg.Publish.Payload)
+			case <-ticker.C:
+				//cfg.Publish.Payload["time"] = t.Unix()
+				payload := cfg.Publish.Payload
+				temp := template.New("timer")
+				temp, err := temp.Parse(payload)
 				if err != nil {
-					return fmt.Errorf("Failed to marshal: %s", err.Error())
+					return err
 				}
+				//assert.NoError(ctx, err)
+				kk := new(bytes.Buffer)
+				err = temp.Execute(kk, &cx{})
+				if err != nil {
+					return err
+				}
+				ss := kk.String()
 				pkt := packet.NewPublish()
 				pkt.Message.Topic = cfg.Publish.Topic
 				pkt.Message.QOS = packet.QOS(cfg.Publish.QOS)
-				pkt.Message.Payload = pld
+				pkt.Message.Payload = []byte(ss)
 				// send a message to hub triggered by timer
 				err = cli.Send(pkt)
 				if err != nil {
