181ef2727d9079256ea6054de7db51aeb5dd9b62
cs1961955069 <1961955069@qq.com>
2019-12-29 14:04:39 +0800

    Create pack.sh

---
+++ scripts/centos/pack.sh
@@ -0,0 +1,14 @@
+#!/bin/bash
+
+set -e
+
+yum update -y && yum install -y rpmdevtools rpm-sign
+
+mkdir -p ~/rpmbuild/RPMS ~/rpmbuild/SRPMS ~/rpmbuild/BUILD ~/rpmbuild/SOURCES
+
+cp baetyl-@version@.tar.gz ~/rpmbuild/SOURCES
+
+cp scripts/centos/baetyl.spec ~/rpmbuild/SPECS/baetyl.spec
+
+rpmbuild -v -bb --clean ~/rpmbuild/SPECS/baetyl.spec
+
