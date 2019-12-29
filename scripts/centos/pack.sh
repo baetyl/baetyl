1dfd8d3e432cc87544b461dc44e6e8b38b78de6b
cs1961955069 <1961955069@qq.com>
2019-12-29 15:17:28 +0800

    Update pack.sh

--- scripts/centos/pack.sh
+++ scripts/centos/pack.sh
@@ -14,5 +14,14 @@ cp scripts/centos/baetyl.spec ~/rpmbuild/SPECS/baetyl.spec
 
 rpmbuild -v -bb --clean ~/rpmbuild/SPECS/baetyl.spec
 
+echo "yes" | setsid rpm \
+    --define "_gpg_name baetyl" \
+    --define "_signature gpg" \
+    --define "__gpg_check_password_cmd /bin/true" \
+    --define "__gpg_sign_cmd %{__gpg} gpg --batch --no-armor --digest-algo 'sha512' --passphrase '@passphrase@' --no-secmem-warning -u '%{_gpg_name}' --sign --detach-sign --output %{__signature_filename} %{__plaintext_filename}" \
+    --resign "~/rpmbuild/RPMS/x86_64/$(ls ~/rpmbuild/RPMS/x86_64)"
+
 cp ~/rpmbuild/RPMS/x86_64/$(ls ~/rpmbuild/RPMS/x86_64/) .
+
+exit 0
 
