4ed4f40fcb9e2ce00e9d8ee82f49d826b0832733
cs1961955069 <1961955069@qq.com>
2019-12-29 14:22:30 +0800

    Update pack.sh

--- scripts/centos/pack.sh
+++ scripts/centos/pack.sh
@@ -2,7 +2,7 @@
 
 set -e
 
-yum update -y && yum install -y rpmdevtools rpm-sign
+yum update -y && yum install -y rpmdevtools rpm-sign tree
 
 mkdir -p ~/rpmbuild/RPMS ~/rpmbuild/SRPMS ~/rpmbuild/BUILD ~/rpmbuild/SOURCES ~/rpmbuild/SPECS
 
@@ -11,4 +11,6 @@ cp baetyl-@version@.tar.gz ~/rpmbuild/SOURCES
 cp scripts/centos/baetyl.spec ~/rpmbuild/SPECS/baetyl.spec
 
 rpmbuild -v -bb --clean ~/rpmbuild/SPECS/baetyl.spec
+
+tree ~rpmbuild/
 
