c0b1fe870b55e4447d530d7915481bffae81cdcf
zhangdong01 <zhangdong01@gridsum.com>
2020-04-20 16:31:15 +0800

    Revert "解决容器失联问题"
    This reverts commit 1d41b25a

--- master/service.go
+++ master/service.go
@@ -6,7 +6,7 @@ import (
 	"sync"
 
 	"github.com/baetyl/baetyl/master/engine"
-	baetyl "github.com/baetyl/baetyl/sdk/baetyl-go"
+	"github.com/baetyl/baetyl/sdk/baetyl-go"
 	"github.com/docker/distribution/uuid"
 )
 
