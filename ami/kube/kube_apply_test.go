e8da036ae4272ceb54c6356db58d73e80c7a2fd6
zhaoxin <zhaoxin26@baidu.com>
2021-11-19 15:32:58 +0800

    support containers (#673)
    * baetyl-1404 support containers
    
    * baetyl-1404 fix clean and clean_test
    
    * baetyl-1404 add kube apply v2 test
    
    * baetyl-1404 add kube apply v2 test
    
    * baetyl-1404 add compatible test

--- ami/kube/kube_apply_test.go
+++ ami/kube/kube_apply_test.go
@@ -48,218 +48,6 @@ func TestCheckAndCreateNamespace(t *testing.T) {
 	assert.NoError(t, err)
 }
 
-func TestApplyApplication(t *testing.T) {
-	ami := initApplyKubeAMI(t)
-	ns := "baetyl-edge"
-	aname := "app1"
-	ver := "a1"
-	sname := "svc1"
-	app := specv1.Application{
-		Name:      aname,
-		Namespace: ns,
-		Version:   ver,
-		Services: []specv1.Service{{
-			Name:  sname,
-			Image: "image1",
-			Ports: []specv1.ContainerPort{{
-				HostPort:      80,
-				ContainerPort: 80,
-			}},
-		}},
-		Volumes: []specv1.Volume{{
-			Name:         "cfg1",
-			VolumeSource: specv1.VolumeSource{Config: &specv1.ObjectReference{Name: "cfg1", Version: "c1"}},
-		}, {
-			Name:         "sec1",
-			VolumeSource: specv1.VolumeSource{Secret: &specv1.ObjectReference{Name: "sec1", Version: "s1"}},
-		}},
-	}
-	secs := []string{"sec1"}
-	err := ami.applyApplication(ns, app, secs)
-	assert.NoError(t, err)
-}
-
-func TestPrepareService(t *testing.T) {
-	ami := initApplyKubeAMI(t)
-	svcName := "svc"
-	ns := "baetyl-edge"
-	svc := &specv1.Service{
-		Name:  svcName,
-		Ports: []specv1.ContainerPort{{ContainerPort: 80}, {ContainerPort: 8080}},
-	}
-	service := ami.prepareService(ns, "", svc)
-	expected := &v1.Service{
-		ObjectMeta: metav1.ObjectMeta{Name: svcName, Namespace: ns, Labels: map[string]string{AppName: ""}},
-		Spec: v1.ServiceSpec{
-			Ports: []v1.ServicePort{{
-				Name:       "svc-0",
-				Port:       80,
-				TargetPort: intstr.IntOrString{IntVal: 80},
-			}, {
-				Name:       "svc-1",
-				Port:       8080,
-				TargetPort: intstr.IntOrString{IntVal: 8080},
-			}},
-			Selector: map[string]string{
-				ServiceName: svcName,
-			},
-		},
-	}
-	assert.Equal(t, service, expected)
-	svc = &specv1.Service{
-		Name: svcName,
-	}
-	service = ami.prepareService(ns, "", svc)
-	assert.Nil(t, service)
-}
-
-func TestPrepareDeploy(t *testing.T) {
-	_ = initApplyKubeAMI(t)
-	ns := "baetyl-edge"
-	svcName := "svc"
-	app := specv1.Application{
-		Name:      "app",
-		Namespace: ns,
-		Version:   "a1",
-	}
-	svc := specv1.Service{
-		Name:    svcName,
-		Replica: 1,
-		VolumeMounts: []specv1.VolumeMount{{
-			Name: "cfg",
-		}, {
-			Name: "sec",
-		}, {
-			Name: "hostPath",
-		}},
-		Resources: &specv1.Resources{
-			Limits: map[string]string{
-				"cpu":    "1",
-				"memory": "456456",
-			},
-		},
-	}
-	cpuQuan, _ := resource.ParseQuantity("1")
-	memoryQuan, _ := resource.ParseQuantity("456456")
-	app.Volumes = []specv1.Volume{{
-		Name: "cfg",
-		VolumeSource: specv1.VolumeSource{
-			Config: &specv1.ObjectReference{
-				Name: "cfg",
-			},
-		},
-	}, {
-		Name: "sec",
-		VolumeSource: specv1.VolumeSource{
-			Secret: &specv1.ObjectReference{
-				Name: "sec",
-			},
-		},
-	}, {
-		Name: "hostPath",
-		VolumeSource: specv1.VolumeSource{
-			HostPath: &specv1.HostPathVolumeSource{Path: "/var/lib/baetyl"},
-		},
-	}}
-	deploy, err := prepareDeploy(ns, &app, svc, nil)
-	assert.NoError(t, err)
-	replica := new(int32)
-	*replica = 1
-	expected := &appv1.Deployment{
-		ObjectMeta: metav1.ObjectMeta{
-			Name:      svcName,
-			Namespace: ns,
-			Labels: map[string]string{
-				AppName:     app.Name,
-				AppVersion:  app.Version,
-				ServiceName: svcName,
-			},
-		},
-		Spec: appv1.DeploymentSpec{
-			Replicas: replica,
-			Selector: &metav1.LabelSelector{
-				MatchLabels: map[string]string{
-					ServiceName: svcName,
-				},
-			},
-			Template: v1.PodTemplateSpec{
-				ObjectMeta: metav1.ObjectMeta{
-					Labels: map[string]string{
-						ServiceName: svcName,
-					},
-				},
-				Spec: v1.PodSpec{
-					Affinity: &v1.Affinity{
-						NodeAffinity: &v1.NodeAffinity{
-							RequiredDuringSchedulingIgnoredDuringExecution: &v1.NodeSelector{
-								NodeSelectorTerms: []v1.NodeSelectorTerm{{MatchExpressions: []v1.NodeSelectorRequirement{
-									{
-										Key:      MasterRole,
-										Operator: v1.NodeSelectorOpExists,
-									},
-								}}},
-							},
-						},
-					},
-					Volumes: []v1.Volume{
-						{
-							Name: "cfg",
-							VolumeSource: v1.VolumeSource{
-								ConfigMap: &v1.ConfigMapVolumeSource{
-									LocalObjectReference: v1.LocalObjectReference{Name: "cfg"},
-								},
-							},
-						},
-						{
-							Name: "sec",
-							VolumeSource: v1.VolumeSource{
-								Secret: &v1.SecretVolumeSource{
-									SecretName: "sec",
-								},
-							},
-						},
-						{
-							Name: "hostPath",
-							VolumeSource: v1.VolumeSource{
-								HostPath: &v1.HostPathVolumeSource{
-									Path: "/var/lib/baetyl",
-								},
-							},
-						},
-					},
-					Containers: []v1.Container{{
-						Env: []v1.EnvVar{
-							{
-								Name:      KubeNodeName,
-								ValueFrom: &v1.EnvVarSource{FieldRef: &v1.ObjectFieldSelector{FieldPath: "spec.nodeName"}},
-							},
-						},
-						Name: "svc",
-						Resources: v1.ResourceRequirements{
-							Limits: v1.ResourceList{
-								"cpu":    cpuQuan,
-								"memory": memoryQuan,
-							},
-							Requests: v1.ResourceList{},
-						},
-						VolumeMounts: []v1.VolumeMount{{
-							Name: "cfg",
-						}, {
-							Name: "sec",
-						}, {
-							Name: "hostPath",
-						}},
-					}},
-				},
-			},
-			Strategy: appv1.DeploymentStrategy{
-				Type: appv1.RecreateDeploymentStrategyType,
-			},
-		},
-	}
-	assert.Equal(t, expected, deploy)
-}
-
 func TestApplyDeploys(t *testing.T) {
 	ami := initApplyKubeAMI(t)
 	ns := "baetyl-edge"
@@ -321,13 +109,13 @@ func TestApplyDaemons(t *testing.T) {
 func TestDeleteApplication(t *testing.T) {
 	ami := initApplyKubeAMI(t)
 	ns := "baetyl-edge"
-	sname := "svc1"
+	name := "svc1"
 	app := specv1.Application{
-		Name:      "app1",
+		Name:      name,
 		Namespace: ns,
 		Version:   "a1",
 		Services: []specv1.Service{{
-			Name:  sname,
+			Name:  "s1",
 			Image: "image1",
 			Ports: []specv1.ContainerPort{{
 				HostPort:      80,
@@ -337,18 +125,18 @@ func TestDeleteApplication(t *testing.T) {
 	}
 	err := ami.applyApplication(ns, app, nil)
 	assert.NoError(t, err)
-	d, err := ami.cli.app.Deployments(ns).Get(sname, metav1.GetOptions{})
+	d, err := ami.cli.app.Deployments(ns).Get(name, metav1.GetOptions{})
 	assert.NotNil(t, d)
 	assert.NoError(t, err)
-	s, err := ami.cli.core.Services(ns).Get(sname, metav1.GetOptions{})
+	s, err := ami.cli.core.Services(ns).Get(name, metav1.GetOptions{})
 	assert.NotNil(t, s)
 	assert.NoError(t, err)
 
 	err = ami.deleteApplication(ns, app.Name)
 	assert.NoError(t, err)
-	d, _ = ami.cli.app.Deployments(ns).Get(sname, metav1.GetOptions{})
+	d, _ = ami.cli.app.Deployments(ns).Get(name, metav1.GetOptions{})
 	assert.Nil(t, d)
-	s, _ = ami.cli.core.Services(ns).Get(sname, metav1.GetOptions{})
+	s, _ = ami.cli.core.Services(ns).Get(name, metav1.GetOptions{})
 	assert.Nil(t, s)
 }
 
@@ -535,4 +323,366 @@ func initApplyKubeAMI(t *testing.T) *kubeImpl {
 	assert.NotNil(t, sto)
 	ami.Hooks[BaetylSetPodSpec] = SetPodSpecFunc(SetPodSpec)
 	return &kubeImpl{cli: &cli, store: sto, knn: "node1", log: log.With()}
+}
+
+func TestApplyApplicationV2(t *testing.T) {
+	ami := initApplyKubeAMI(t)
+	ns := "baetyl-edge"
+	app1 := specv1.Application{
+		Name:      "svc1",
+		Namespace: ns,
+		Version:   "a1",
+		Services: []specv1.Service{
+			{
+				Name:  "s1",
+				Image: "image1",
+				Ports: []specv1.ContainerPort{{
+					HostPort:      80,
+					ContainerPort: 80,
+				}},
+			},
+			{
+				Name:  "s2",
+				Image: "image1",
+				Ports: []specv1.ContainerPort{{
+					HostPort:      80,
+					ContainerPort: 80,
+				}},
+			},
+		},
+		Volumes: []specv1.Volume{{
+			Name:         "cfg1",
+			VolumeSource: specv1.VolumeSource{Config: &specv1.ObjectReference{Name: "cfg1", Version: "c1"}},
+		}, {
+			Name:         "sec1",
+			VolumeSource: specv1.VolumeSource{Secret: &specv1.ObjectReference{Name: "sec1", Version: "s1"}},
+		}},
+	}
+	secs := []string{"sec1"}
+	err := ami.applyApplication(ns, app1, secs)
+	assert.NoError(t, err)
+
+	app2 := specv1.Application{
+		Name:      "svc1",
+		Namespace: ns,
+		Version:   "a2",
+		Services: []specv1.Service{
+			{
+				Name:  "s1",
+				Image: "image1",
+				Type:  specv1.WorkloadDaemonSet,
+				Ports: []specv1.ContainerPort{{
+					HostPort:      80,
+					ContainerPort: 80,
+				}},
+			},
+		},
+		Volumes: []specv1.Volume{{
+			Name:         "cfg1",
+			VolumeSource: specv1.VolumeSource{Config: &specv1.ObjectReference{Name: "cfg1", Version: "c1"}},
+		}, {
+			Name:         "sec1",
+			VolumeSource: specv1.VolumeSource{Secret: &specv1.ObjectReference{Name: "sec1", Version: "s1"}},
+		}},
+	}
+	err = ami.applyApplication(ns, app2, secs)
+	assert.NoError(t, err)
+
+	app3 := specv1.Application{
+		Name:      "svc1",
+		Namespace: ns,
+		Version:   "a2",
+		Workload:  specv1.WorkloadJob,
+		Services: []specv1.Service{
+			{
+				Name:  "s1",
+				Image: "image1",
+			},
+		},
+		Volumes: []specv1.Volume{{
+			Name:         "sec1",
+			VolumeSource: specv1.VolumeSource{Secret: &specv1.ObjectReference{Name: "sec1", Version: "s1"}},
+		}},
+	}
+	err = ami.applyApplication(ns, app3, secs)
+	assert.NoError(t, err)
+}
+
+func TestPrepareServiceV2(t *testing.T) {
+	ami := initApplyKubeAMI(t)
+	ns := "baetyl-edge"
+	app := specv1.Application{
+		Name:      "svc",
+		Namespace: ns,
+		Version:   "a1",
+		Services: []specv1.Service{
+			{
+				Name:  "s1",
+				Image: "image1",
+				Ports: []specv1.ContainerPort{{
+					HostPort:      80,
+					ContainerPort: 80,
+				}},
+			},
+			{
+				Name:  "s2",
+				Image: "image1",
+				Ports: []specv1.ContainerPort{{
+					HostPort:      80,
+					ContainerPort: 8080,
+				}},
+			},
+		},
+	}
+	service := ami.prepareService(ns, app)
+	expected := &v1.Service{
+		ObjectMeta: metav1.ObjectMeta{Name: app.Name, Namespace: ns, Labels: map[string]string{AppName: app.Name}},
+		Spec: v1.ServiceSpec{
+			Ports: []v1.ServicePort{{
+				Name:       "s1-0",
+				Port:       80,
+				TargetPort: intstr.IntOrString{IntVal: 80},
+			}, {
+				Name:       "s2-0",
+				Port:       8080,
+				TargetPort: intstr.IntOrString{IntVal: 8080},
+			}},
+			Selector: map[string]string{
+				AppName: app.Name,
+			},
+		},
+	}
+	assert.Equal(t, service, expected)
+
+	// bad case
+	app2 := specv1.Application{
+		Name:      "app2",
+		Namespace: ns,
+		Version:   "a1",
+		Services: []specv1.Service{
+			{Name: "svc1"},
+		},
+	}
+	service = ami.prepareService(ns, app2)
+	assert.Nil(t, service)
+}
+
+func TestPrepareDeployV2(t *testing.T) {
+	_ = initApplyKubeAMI(t)
+	ns := "baetyl-edge"
+	app := specv1.Application{
+		Name:      "svc",
+		Namespace: ns,
+		Version:   "a1",
+		Replica:   1,
+		Services: []specv1.Service{
+			{
+				Name: "s0",
+				VolumeMounts: []specv1.VolumeMount{{
+					Name: "cfg",
+				}, {
+					Name: "sec",
+				}, {
+					Name: "hostPath",
+				}},
+				Resources: &specv1.Resources{
+					Limits: map[string]string{
+						"cpu":    "1",
+						"memory": "456456",
+					},
+				},
+			},
+		},
+	}
+	cpuQuan, _ := resource.ParseQuantity("1")
+	memoryQuan, _ := resource.ParseQuantity("456456")
+	app.Volumes = []specv1.Volume{{
+		Name: "cfg",
+		VolumeSource: specv1.VolumeSource{
+			Config: &specv1.ObjectReference{
+				Name: "cfg",
+			},
+		},
+	}, {
+		Name: "sec",
+		VolumeSource: specv1.VolumeSource{
+			Secret: &specv1.ObjectReference{
+				Name: "sec",
+			},
+		},
+	}, {
+		Name: "hostPath",
+		VolumeSource: specv1.VolumeSource{
+			HostPath: &specv1.HostPathVolumeSource{Path: "/var/lib/baetyl"},
+		},
+	}}
+	deploy, err := prepareDeploy(ns, &app, nil)
+	assert.NoError(t, err)
+
+	replica := new(int32)
+	*replica = 1
+	expected := &appv1.Deployment{
+		ObjectMeta: metav1.ObjectMeta{
+			Name:      app.Name,
+			Namespace: ns,
+			Labels:    map[string]string{},
+		},
+		Spec: appv1.DeploymentSpec{
+			Replicas: replica,
+			Selector: &metav1.LabelSelector{
+				MatchLabels: map[string]string{
+					AppName: app.Name,
+				},
+			},
+			Template: v1.PodTemplateSpec{
+				ObjectMeta: metav1.ObjectMeta{
+					Labels: map[string]string{
+						AppName: app.Name,
+					},
+				},
+				Spec: v1.PodSpec{
+					Affinity: &v1.Affinity{
+						NodeAffinity: &v1.NodeAffinity{
+							RequiredDuringSchedulingIgnoredDuringExecution: &v1.NodeSelector{
+								NodeSelectorTerms: []v1.NodeSelectorTerm{{MatchExpressions: []v1.NodeSelectorRequirement{
+									{
+										Key:      MasterRole,
+										Operator: v1.NodeSelectorOpExists,
+									},
+								}}},
+							},
+						},
+					},
+					Volumes: []v1.Volume{
+						{
+							Name: "cfg",
+							VolumeSource: v1.VolumeSource{
+								ConfigMap: &v1.ConfigMapVolumeSource{
+									LocalObjectReference: v1.LocalObjectReference{Name: "cfg"},
+								},
+							},
+						},
+						{
+							Name: "sec",
+							VolumeSource: v1.VolumeSource{
+								Secret: &v1.SecretVolumeSource{
+									SecretName: "sec",
+								},
+							},
+						},
+						{
+							Name: "hostPath",
+							VolumeSource: v1.VolumeSource{
+								HostPath: &v1.HostPathVolumeSource{
+									Path: "/var/lib/baetyl",
+								},
+							},
+						},
+					},
+					Containers: []v1.Container{{
+						Env: []v1.EnvVar{
+							{
+								Name:      KubeNodeName,
+								ValueFrom: &v1.EnvVarSource{FieldRef: &v1.ObjectFieldSelector{FieldPath: "spec.nodeName"}},
+							},
+						},
+						Name: "s0",
+						Resources: v1.ResourceRequirements{
+							Limits: v1.ResourceList{
+								"cpu":    cpuQuan,
+								"memory": memoryQuan,
+							},
+							Requests: v1.ResourceList{},
+						},
+						VolumeMounts: []v1.VolumeMount{{
+							Name: "cfg",
+						}, {
+							Name: "sec",
+						}, {
+							Name: "hostPath",
+						}},
+					}},
+				},
+			},
+			Strategy: appv1.DeploymentStrategy{
+				Type: appv1.RecreateDeploymentStrategyType,
+			},
+		},
+	}
+	assert.Equal(t, expected, deploy)
+}
+
+func Test_compatibleDeprecatedFiled(t *testing.T) {
+	k := initApplyKubeAMI(t)
+	// case 0
+	app0 := &specv1.Application{
+		Name: "a0",
+		Services: []specv1.Service{{
+			Name:        "s1",
+			Labels:      map[string]string{"a": "b"},
+			HostNetwork: true,
+			Replica:     3,
+			JobConfig: &specv1.ServiceJobConfig{
+				Completions:   1,
+				Parallelism:   2,
+				BackoffLimit:  3,
+				RestartPolicy: "Never",
+			},
+			Type: specv1.WorkloadJob,
+		}},
+	}
+
+	expectApp0 := &specv1.Application{
+		Name:        "a0",
+		Labels:      map[string]string{"a": "b"},
+		HostNetwork: true,
+		Replica:     3,
+		JobConfig: &specv1.AppJobConfig{
+			Completions:   1,
+			Parallelism:   2,
+			BackoffLimit:  3,
+			RestartPolicy: "Never",
+		},
+		Workload: specv1.WorkloadJob,
+		Services: []specv1.Service{{
+			Name:        "s1",
+			Labels:      map[string]string{"a": "b"},
+			HostNetwork: true,
+			Replica:     3,
+			JobConfig: &specv1.ServiceJobConfig{
+				Completions:   1,
+				Parallelism:   2,
+				BackoffLimit:  3,
+				RestartPolicy: "Never",
+			},
+			Type: specv1.WorkloadJob,
+		}},
+	}
+
+	k.compatibleDeprecatedFiled(app0)
+	assert.EqualValues(t, expectApp0, app0)
+
+	// case 1
+	app1 := &specv1.Application{
+		Name:     "a1",
+		Services: []specv1.Service{},
+	}
+
+	expectApp1 := &specv1.Application{
+		Name:        "a1",
+		Labels:      map[string]string{},
+		HostNetwork: false,
+		Replica:     1,
+		JobConfig: &specv1.AppJobConfig{
+			Completions:   0,
+			Parallelism:   0,
+			BackoffLimit:  0,
+			RestartPolicy: "Never",
+		},
+		Workload: specv1.WorkloadDeployment,
+		Services: []specv1.Service{},
+	}
+
+	k.compatibleDeprecatedFiled(app1)
+	assert.EqualValues(t, expectApp1, app1)
 }
